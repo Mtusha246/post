0441bf89d01f40a11cbda5520d347ba783d81220
Mtusha246 <gicmatveialputov@gmail.com>
2025-11-01 19:50:48 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,8 +1,11 @@
+// db.js
 const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
+  ssl: {
+    rejectUnauthorized: false, // важно для Railway
+  },
 });
 
 module.exports = pool;
