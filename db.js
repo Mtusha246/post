96bb88aeca9760bc89938e427c1d10ca77790c97
Mtusha246 <gicmatveialputov@gmail.com>
2025-11-01 19:14:14 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,10 +1,8 @@
-// db.js
 const { Pool } = require('pg');
-require('dotenv').config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false } // Railway требует SSL
+  ssl: { rejectUnauthorized: false },
 });
 
 module.exports = pool;
