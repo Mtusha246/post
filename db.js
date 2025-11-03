b4d7be5c3b8c1717dd51bd389b00bbd243cd5bdd
Mtusha246 <gicmatveialputov@gmail.com>
2025-11-03 13:08:30 +0530

    Update db.js

--- db.js
+++ db.js
@@ -2,10 +2,15 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false, // важно для Railway
-  },
+  connectionString:
+    process.env.DATABASE_URL ||
+    'postgresql://postgres:gjbLXHghHdItlgjBWudmyhfESlrbsPke@caboose.proxy.rlwy.net:19817/railway',
+  ssl: { rejectUnauthorized: false },
 });
+
+pool
+  .connect()
+  .then(() => console.log('✅ Connected to PostgreSQL via db.js'))
+  .catch(err => console.error('❌ DB connection error:', err.message));
 
 module.exports = pool;
